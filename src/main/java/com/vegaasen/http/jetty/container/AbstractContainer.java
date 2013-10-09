7a23b7ba6f55015ecf78ee95c9896b5ae8b03545
vegaasen <vegaasen@gmail.com>
2013-10-09 22:07:37 +0200

    First work for basicAuth

--- src/main/java/com/vegaasen/http/jetty/container/AbstractContainer.java
+++ src/main/java/com/vegaasen/http/jetty/container/AbstractContainer.java
@@ -1,6 +1,14 @@
 package com.vegaasen.http.jetty.container;
 
+import com.vegaasen.http.jetty.model.JettyArguments;
+import com.vegaasen.http.jetty.model.User;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Credential;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * @author <a href="vegard.aasen@gmail.com">vegardaasen</a>
@@ -24,4 +32,33 @@ public abstract class AbstractContainer implements Container {
     public boolean isRunning() {
         throw new IllegalStateException("Not implemented");
     }
+
+    protected void setupBasicAuthentication(final WebAppContext context, final JettyArguments args) {
+        if(context==null) {
+            return;
+        }
+        if(args==null) {
+            return;
+        }
+        final Constraint constraint = new Constraint();
+        constraint.setName(Constraint.__BASIC_AUTH);
+        constraint.setRoles(args.getUserRoles());
+        constraint.setAuthenticate(true);
+
+        final ConstraintMapping basicAuthMapping = new ConstraintMapping();
+        basicAuthMapping.setConstraint(constraint);
+        basicAuthMapping.setPathSpec(args.getProtectedPath());
+
+        final HashLoginService loginService = new HashLoginService(args.getRealm());
+        for(User user : args.getAllowedUsers()) {
+            loginService.putUser(user.getUsername(), Credential.getCredential(user.getPassword()), args.getUserRoles());
+        }
+        final ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
+        securityHandler.setLoginService(loginService);
+
+        securityHandler.addConstraintMapping(basicAuthMapping);
+
+        context.setHandler(securityHandler);
+    }
+
 }
--- src/main/java/com/vegaasen/http/jetty/model/JettyArguments.java
+++ src/main/java/com/vegaasen/http/jetty/model/JettyArguments.java
@@ -7,15 +7,20 @@ public final class JettyArguments {
 
     public static final String[] JETTY_USER_ROLES = new String[]{"user"};
     public static final String[] JETTY_PROTOCOLS = new String[]{"http/1.1"};
+    public static final String DEFAULT_PATH = "/";
     public static final int DEFAULT_PORT = 7000;
     public static final int DEFAULT_CONTROL_PORT = DEFAULT_PORT + 1;
     public static final int DEFAULT_HTTPS_PORT = DEFAULT_PORT + 443;
+    public static final User DEFAULT_USER = new User.Builder().username("user").password("password").build();
 
     private String[] protocols = JETTY_PROTOCOLS;
-    private String root = "/";
-    private String controlRoot = "/";
-    private User[] allowedUsers;
+    private String root = DEFAULT_PATH;
+    private String controlRoot = DEFAULT_PATH;
+    private User[] allowedUsers = new User[]{DEFAULT_USER};
     private String[] userRoles = JETTY_USER_ROLES;
+    private String protectedPath = DEFAULT_PATH + "*";
+    private String unprotectedPaths;
+    private String realm = "Jetty Application Server";
 
     public JettyArguments() {
     }
@@ -58,5 +63,21 @@ public final class JettyArguments {
 
     public void setUserRoles(String[] userRoles) {
         this.userRoles = userRoles;
+    }
+
+    public String getProtectedPath() {
+        return protectedPath;
+    }
+
+    public void setProtectedPath(String protectedPath) {
+        this.protectedPath = protectedPath;
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public void setRealm(String realm) {
+        this.realm = realm;
     }
 }
